f609bc1101d77c4a7c8924a473009bbdd6366f1a
Andron1976 <and.ron@ukr.net>
2022-06-06 15:02:05 +0300

    did task 9.1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,28 @@
+const refs = {
+    startButton: document.querySelector('button[data-start]'),
+    stopButton: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body'),
+  };
+console.log(refs.startButton);
+console.log(refs.stopButton);
+let intervalId = null;
+refs.stopButton.disabled = true;
+
+function getRandomColor() {
+    return `${Math.floor(Math.random()*16777215).toString(16)}`
+};
+
+refs.startButton.addEventListener('click', () => {
+    refs.startButton.disabled = true;
+    refs.stopButton.disabled = false;
+
+    intervalId = setInterval(() => {
+        refs.body.style.backgroundColor = `${getRandomColor()}`;
+    }, 1000);
+})
+
+refs.stopButton.addEventListener('click', () => {
+    clearInterval(intervalId);
+    refs.startButton.disabled = false;
+    refs.stopButton.disabled = true;
+});
