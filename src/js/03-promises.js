b6c2f0708aeb64edb76a39f1dd0adba6991bc280
Andron1976 <and.ron@ukr.net>
2022-06-07 08:08:04 +0300

    did task 9.3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', didSubmit);
+
+function didSubmit(el) {
+  el.preventDefault();
+
+  const inputItems = el.currentTarget.elements;
+
+  let amount = Number(inputItems.amount.value);
+  let delay = Number(inputItems.delay.value);
+  let step = Number(inputItems.step.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+    .then(({ i, delay }) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${i} in ${delay}ms`);
+    })
+    .catch(({ i, delay }) => {
+      Notiflix.Notify.failure(`❌ Rejected promise ${i} in ${delay}ms`);
+    });
+    delay += step;
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setInterval(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
